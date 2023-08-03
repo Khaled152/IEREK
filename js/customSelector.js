f1893cc3b65ac2f5d3ad56518cc9433c965dedf4
Khaled152 <khaled.ahmed1512199@gmail.com>
2023-08-03 17:39:10 +0300

    Abstract & paper Pages

--- js/customSelector.js
+++ js/customSelector.js
@@ -49,20 +49,70 @@ function chnageSelectedItem(item) {
     activeLinkFor(checkedItem.parentElement.getAttribute("for"))
 }
 
-function chnageActiveLink(link){
+function chnageActiveLink(link) {
     activeNavItem.classList.remove("active")
     activeNavItem = link
     activeNavItem.classList.add("active")
 }
 
-function activeLinkFor(searchedLink){
-    for(link of links){        
-         href = link.href.split("#")[1]         
-         if(href == searchedLink){
+function activeLinkFor(searchedLink) {
+    for (link of links) {
+        href = link.href.split("#")[1]
+        if (href == searchedLink) {
             chnageActiveLink(link)
             break
-         }
+        }
     }
 }
 
-/***********************test***********************/
+/***********************Muilte selector***********************/
+let dropdown = document.querySelector(".multiselect-dropdown");
+let menu = document.querySelector(".menu");
+let listContainer = document.querySelector(".selectedList-container");
+let selectedItems = [];
+
+dropdown.onclick = (event) => {
+    event.currentTarget.classList.toggle("show");
+};
+
+menu.addEventListener("click", (event) => {
+    event.stopPropagation();
+
+    let item = event.target.innerHTML;
+
+    if (!selectedItems.includes(item)) {
+        selectedItems.push(item);
+        showSelectedItems(item);
+    }
+});
+
+function showSelectedItems(item) {
+    let itemSpan = document.createElement("span");
+    let crossIcon = document.createElement("i");
+
+    itemSpan.innerHTML = item;
+    itemSpan.classList.add("selectedItem");
+
+    crossIcon.classList.add("fa-solid", "fa-xmark");
+    itemSpan.append(crossIcon);
+
+    itemSpan.addEventListener("click", deleteItem);
+    listContainer.append(itemSpan);
+}
+
+function deleteItem(event) {
+    event.stopPropagation();
+    let item = event.currentTarget;
+
+    selectedItems = selectedItems.filter((value) => value !== item.textContent);
+
+    item.classList.add("zoomOut");
+
+    setTimeout(() => {
+        item.remove();
+        item.classList.remove("zoomOut");
+    }, 390);
+}
+
+/************************************/
+
